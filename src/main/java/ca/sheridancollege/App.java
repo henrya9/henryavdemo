dd976b834ff3314c600c8badcfa971fc1b934090
Ava Henry <ae.henry@live.com>
2025-03-10 21:08:19 -0400

    Add files from java project for lab 3

---
+++ src/main/java/ca/sheridancollege/App.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, this is lab 3" );
+    }
+}
